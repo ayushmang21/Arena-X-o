634f35d3fcea8071704f23c9d6e05a874c02225d
Ayushman Gupta <ayushman0021@gmail.com>
2024-06-13 19:29:01 +0530

    q

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,9 +7,6 @@ import Navbar from './components/Navbar';
 import Paid_tour from './components/Paid_tour';
 import Free_tour from './components/Free_tour';
 import Create_tour from './components/Create_tour';
-import Faq from './components/faq';
-import Leaderboard from './components/leaderboard';
-import Wiki from './components/Wiki';
 import Footer from './components/Footer';
 import Challenges_cash from './components/Challenges_cash';
 import Challenges_xp from './components/Challenges_xp';
@@ -32,9 +29,6 @@ const App = () => {
             <Route path='/manage' element={< ManageTour/>} />
             <Route path='/challenges_cash' element={< Challenges_cash/>} />
             <Route path='/challenges_xp' element={< Challenges_xp/>} />
-            {/* <Route path='/wiki' element={< Wiki/>} /> */}
-            {/* <Route path='/leaderboard' element={< Leaderboard/>} /> */}
-            {/* <Route path='/faq' element={< Faq/>} /> */}
             
 
           </Routes>
