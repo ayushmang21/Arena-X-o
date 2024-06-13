a1596e798456b83a9d273c2d10ccb24fc578050d
Ayushman Gupta <ayushman0021@gmail.com>
2024-06-13 19:18:54 +0530

    Remove unused components: Faq, Leaderboard, Login, Not_Found, Signup, Tournament, and Wiki

--- front/src/components/Not_Found.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Not_Found = () => {
-  return (
-    <div>Not_Found</div>
-  )
-}
-
-export default Not_Found;
--- front/src/components/Wiki.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Wiki = () => {
-  return (
-    <div>Wiki</div>
-  )
-}
-
-export default Wiki;
